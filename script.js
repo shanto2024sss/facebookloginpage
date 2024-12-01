0903a1f9eba5e1a1e2015480451df5f2dc8fc701
shanto2024sss <naimulislamshanto1086@gmail.com>
2024-12-01 15:56:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// name = "shanto"
+// console.log("100=100");
+
+// let x;
+// x = 6;
+// document.getElementById("demo").innerHTML = x;
+
+// var name = "namul islam"
+// let Name = "amir hamza"
+// const name1 = "rakib hassan"
